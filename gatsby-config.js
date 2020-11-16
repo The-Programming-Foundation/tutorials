7bbc77e6331bd691af600f9a5dfa4e45a18e03e0
Jonathan Delgado <jonathanrdelgado@gmail.com>
2020-11-15 18:06:34 -0800

    Rename pathPrefix to match repo name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/learn-content', // Temporary to match GH Pages prefix
+  pathPrefix: '/tutorials', // Temporary to match GH Pages prefix
   siteMetadata: {
     siteUrl: 'https://www.theprogrammingfoundation.org/',
     author: 'Subhajeet Mukherjee',
