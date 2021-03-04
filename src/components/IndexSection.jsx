728e5e27a29a9fa56b74503cabc8e16dfc0edd3b
Emily Yu <emily.yu@bellevuecollege.edu>
2021-03-04 10:32:25 -0800

    old slides

--- src/components/IndexSection.jsx
+++ src/components/IndexSection.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const SectionContainer = styled.div`
   display: flex;
 `;
 
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 3
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
 export default ({ children, title }) => {
   return (
     <>
       <h3>{title}</h3>
-      <SectionContainer>{children}</SectionContainer>
+      <SectionContainer>
+        <Carousel 
+            swipeable={true}
+            responsive={responsive}
+            slidesToSlide={1}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            renderButtonGroupOutside={true}
+            containerClass="carousel-container"
+          >
+          {children}
+        </Carousel>  
+      </SectionContainer>
     </>
   );
 }
