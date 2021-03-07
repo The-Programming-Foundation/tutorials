d44cc86733241dab98e796b5a909fd565cc2a776
Emily Yu <emily.yu@bellevuecollege.edu>
2021-03-07 12:35:33 -0800

    refactor pages for v86 iframe

--- src/components/CodeLabLayout.jsx
+++ src/components/CodeLabLayout.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import IframV86 from './IframeV86';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: '15px',
-    textAlign: 'left',
-    backgroundColor: '#FCF1DD',
-    height: 1000,
-    overflowY: "scroll"
-  },
+  grid: {
+    overflowY: "auto",
+    height: 1000
+  }
 });
 
 export default ({children, title}) => {
@@ -22,13 +18,11 @@ export default ({children, title}) => {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>{children}</Paper>
+        <Grid item xs={12} sm={6} className={classes.grid}>
+          {children}
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-              <IframV86 title={title}></IframV86>
-          </Paper>
+          <IframV86 title={title}></IframV86>
         </Grid>  
       </Grid>
     </div>
--- src/components/IframeV86.jsx
+++ src/components/IframeV86.jsx
@@ -4,7 +4,7 @@ export default ({title}) => {
   return (
     <div>
       <p><b>Try it yourself:</b></p>  
-      <iframe title={title} src="https://sad-raman-a7139b.netlify.app/" width="600" height="1000"></iframe>
+      <iframe title={title} src="https://peaceful-bohr-249ea0.netlify.app/" width="600" height="1000"></iframe>
     </div>
   );
 }
