630c53e4705ab17fe36be1eae2afc40b44950c41
yu-emily <49451794+yu-emily@users.noreply.github.com>
2021-03-08 08:11:03 -0800

    took out path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/tutorials', // Temporary to match GH Pages prefix
+  //no needed for custome domains pathPrefix: '/tutorials', // Temporary to match GH Pages prefix
   siteMetadata: {
     siteUrl: 'https://www.theprogrammingfoundation.org/',
     author: 'The Programming Foundation',
