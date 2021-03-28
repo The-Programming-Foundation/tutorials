9365cc4ffd1c97694e27e4c71ce8b12e920ccac4
Subhajeet Mukherjee <subha@smarterus.org>
2021-03-28 20:31:17 +0530

    updates suited for the new iso

--- src/components/IframeV86.jsx
+++ src/components/IframeV86.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({title}) => {
   return (
     <div>
-      <p><b>Try it yourself:</b></p>  
+      <p><b>Try it (press enter when asked):</b></p>  
       <iframe title={title} src="https://peaceful-bohr-249ea0.netlify.app/" width="600" height="1000"></iframe>
     </div>
   );
