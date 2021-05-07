455a1cbca0014119f5271a27f7adc8b2a2270389
Hila Caspi <caspi.home@gmail.com>
2021-05-06 19:04:03 -0700

    add volume transition when pausing sound

--- src/layout/AudioButton.jsx
+++ src/layout/AudioButton.jsx
@@ -8,23 +8,17 @@ const AudioButton = () => {
     const [showModal, setShowModal] = useState(false);
     const toggleModal = () => setShowModal(!showModal);
 
-    // displaying modal only during the user's first visit to site
-    useEffect(() => {
-        let modalViewed = Number(localStorage.getItem('modalViewed')) || 0;
-        if (modalViewed === 0) {
-            toggleModal();
-            localStorage.setItem('modalViewed', 1);
-        }
-    });
-
+    // accessing global state redux rematch
     const audioModel = useSelector(state => state.audioModel);
-    const { audioIsPlaying } = audioModel;
+    const { audioIsPlaying, easeOutMusic } = audioModel;
     const dispatch = useDispatch();
+
 
     if (typeof Audio != "undefined") {
         // browser-only code to enable server-side compiling
         var audio = new Audio(learnSound);
     }
+
     const audioRef = useRef(audio);
 
     // start music when user click on initial modal
@@ -44,10 +38,40 @@ const AudioButton = () => {
         audioRef.current.play();
     }
 
-    const pauseMusic = () => {
-        dispatch.audioModel.toggle();
-        audioRef.current.pause();
-    }
+    const pauseMusic = useRef(null);
+    const [vol, setVol] = useState(1);
+
+    // displaying modal only during the user's first visit to site
+    useEffect(() => {
+        let modalViewed = Number(localStorage.getItem('modalViewed')) || 0;
+        if (modalViewed === 0) {
+            toggleModal();
+            localStorage.setItem('modalViewed', 1);
+        }
+    });
+
+    // fading out music on pause
+    useEffect(() => {
+        if (easeOutMusic === true) {
+            // referencing the current instance of pause music when setTimeout makes an update
+            pauseMusic.current = vol > 0.1 && setTimeout(() => {
+                setVol(vol - 0.05);
+                audioRef.current.volume = vol;
+            }, 100);
+        }
+        return () => {
+            clearTimeout(pauseMusic.current);
+        }
+    }, [vol, easeOutMusic]);
+
+    useEffect(() => {
+        if (vol < 0.1) {
+            dispatch.audioModel.pause();
+            dispatch.audioModel.toggle();
+            audioRef.current.pause();
+            setVol(1);
+        }
+    }, [vol, dispatch.audioModel]);
 
     return (
         <>
@@ -57,7 +81,7 @@ const AudioButton = () => {
                     <div className='modal-learn' >
                         {/* Display mute/unmute buttons when music is playing */}
                         < button type='button' className='mute-btn' onClick={
-                            () => pauseMusic()
+                            () => { dispatch.audioModel.pause() }
                         }> Mute < GoMute /> </button >
                     </div >
                 ) : (
--- src/state/models/audio-model.js
+++ src/state/models/audio-model.js
@@ -3,11 +3,21 @@ import { createModel } from '@rematch/core';
 export const audioModel = createModel({
     state: {
         audioIsPlaying: false,
+        easeOutMusic: false,
     },
     reducers: {
         toggle: (state) => {
             return {
+                ...state,
                 audioIsPlaying: !state.audioIsPlaying
+            }
+        },
+
+        pause: (state) => {
+            return {
+                ...state,
+                easeOutMusic: !state.easeOutMusic
+
             }
         }
     }
