d769301f36e58d766ecb17b98a3cdc10827004b5
Subhajeet Mukherjee <subha@smarterus.org>
2021-05-24 20:44:42 +0530

    update clarity tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         // String value for your clarity project id
         // Project id is found in your clarity dashboard url
         // https://clarity.microsoft.com/projects/view/{clarity_project_id}/
-        clarity_project_id: "6tupxbpkfa",
+        clarity_project_id: "6txkitvfj4",
         // Boolean value for enabling clarity while developing
         // true will enable clarity tracking code on both development and production environments
         // false will enable clarity tracking code on production environment only
