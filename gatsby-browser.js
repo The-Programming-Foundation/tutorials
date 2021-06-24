49117fb4fb9256c02323416a494a7b930649dca4
Hila Caspi <66502157+hcs847@users.noreply.github.com>
2021-06-23 19:39:56 -0700

    Feature/disable sounds (#40)
    * implementing initial version for sticky notes
    
    * triggering note display when reaching the top of viewpoint
    
    * ffix unobserve error for intersection observer
    
    * persist notes and add close button to remove notes
    
    * remove console.logs
    
    * etrigger note modals to be visible only after note is completely hidden at the top
    
    * fix variables for on scroll useEffect
    
    * fix audio faseout on mute
    
    * updating sound fadeout while navigating to another page
    
    * easing out sound when navigating to different lessons
    
    * code refactoring to remove unused portions
    
    * refactoring sounds logic to cover more edge cases
    
    * disabling sound features
    
    * deleting all sound files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,6 @@
 import "@fontsource/poppins" // Defaults to weight 400.
-import AudioButtonWrapper from './src/components/AudioButtonWrapper';
-import React from 'react';
+
 
 // Usage: https://github.com/gatsbyjs/gatsby/tree/master/examples/using-redux
 export { default as wrapRootElement } from "./src/wrapper";
 
-// wraps all pages in the audio button component
-export const wrapPageElement = ({ element, props }) => {
-    return <AudioButtonWrapper {...props}>{element}</AudioButtonWrapper>
-};
