ce269cc2425a2a3223f787e4b532e4966862d86d
Subhajeet Mukherjee <subha@smarterus.org>
2021-11-21 21:14:45 +0530

    update intro to data science

--- src/pages/lessons/getting_started/intro_data_science.jsx
+++ src/pages/lessons/getting_started/intro_data_science.jsx
@@ -13,8 +13,10 @@ const data_science = ({ data: { site, mdx } }) => {
   return (
     <Layout site={site} pageTitle="Intro to Data Science">
       <h1>Intro to Data Science</h1>
-      <h3>Harnessing the power of data opens new possibilities.</h3>
-      <IndexSection title="Core modules">
+      <h3>Data Science careers are one of the most highly compensated careers worldwide currently. Due to its broad application in every industry, there is a higher demand for data scientists who can analyze complex data and communicate the results effectively.
+
+In this course, we will be covering the basics of data science and learning the tools used for data science from scratch. We will start the lessons at the beginner level and move up to the advanced level.</h3>
+      <IndexSection title="Core lessons">
         <IndexItemList lessonTopic={filter('data_science')} />
       </IndexSection>
     </Layout>
