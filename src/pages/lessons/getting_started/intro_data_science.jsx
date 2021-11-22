792a8525003a3a4233f3c0a6b865d2733398e3f8
Subhajeet Mukherjee <subha@smarterus.org>
2021-11-22 17:04:36 +0530

    fix last name

--- src/pages/lessons/getting_started/intro_data_science.jsx
+++ src/pages/lessons/getting_started/intro_data_science.jsx
@@ -16,7 +16,7 @@ const data_science = ({ data: { site, mdx } }) => {
       <h3>Data Science careers are one of the most highly compensated careers worldwide currently. Due to its broad application in every industry, there is a higher demand for data scientists who can analyze complex data and communicate the results effectively.
 
 In this course, we will be covering the basics of data science and learning the tools used for data science from scratch. We will start the lessons at the beginner level and move up to the advanced level.</h3>
-<h5><b>Evolving Series Author:</b> Nalini Nanda | Data Analyst | Content Contributor at The Programming Foundation</h5>
+<h5><b>Evolving Series Author:</b> Nalini Parimi | Data Analyst | Content Contributor at The Programming Foundation</h5>
       <IndexSection title="Core lessons">
         <IndexItemList lessonTopic={filter('data_science')} />
       </IndexSection>
