9ddd6edbcfb6e4aa106aecf52be5522072304bab
Subhajeet Mukherjee <33711869+bootkernel@users.noreply.github.com>
2022-01-10 01:36:42 +0530

    Revert "Feature/tree menu (#42)"
    This reverts commit 6506297a49d6126c78fa4e43df783ff639affa28.

--- src/templates/Lesson.jsx
+++ src/templates/Lesson.jsx
@@ -1,33 +1,31 @@
-import React from "react";
-import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer";
-import { MDXProvider } from "@mdx-js/react";
-import { graphql } from "gatsby";
-import Layout from "../layout/Layout";
-import Note from "../components/Note";
-import CodeEditor from "../components/CodeEditor";
-import CodeLabLayout from "../components/CodeLabLayout";
-import GoBackButton from "../components/treeMenu/GoBackButton";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import MDXRenderer from 'gatsby-plugin-mdx/mdx-renderer';
+import { MDXProvider } from '@mdx-js/react';
+import { graphql } from 'gatsby';
+import Layout from '../layout/Layout';
+import Note from '../components/Note';
+import CodeEditor from '../components/CodeEditor';
+import CodeLabLayout from '../components/CodeLabLayout';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, Row, Col } from "react-bootstrap";
-import "../style/style.css";
+import {Container, Row, Col} from 'react-bootstrap';
+import  '../style/style.css'
 
 const globalComponents = { Note, CodeEditor, CodeLabLayout };
 
 export default function Post({ data: { site, mdx }, pageContext }) {
   return (
+  
     <>
       <Container fluid>
         <Row>
           <Col>
-            <Layout site={site} pageTitle={mdx.frontmatter.title}>
-              {window.innerWidth >= 1000 &&
-                sessionStorage.getItem("lastExpandedNode") && <GoBackButton />}
-              <MDXProvider components={globalComponents}>
-                <MDXRenderer>{pageContext.body}</MDXRenderer>
-              </MDXProvider>
-            </Layout>
-          </Col>
+            <Layout site={site} pageTitle={mdx.frontmatter.title} >
+            <MDXProvider components={globalComponents}>
+              <MDXRenderer>{pageContext.body}</MDXRenderer>
+            </MDXProvider>
+          </Layout>
+        </Col>
         </Row>
       </Container>
     </>
--- src/utils/treeMenu/treeExpansionHelpers.js
+++
@@ -1,4 +0,0 @@
-export function reconstructNodeId(idToArray) {
-  if (idToArray.length <= 1) return idToArray[0];
-  return idToArray.join("-");
-}
