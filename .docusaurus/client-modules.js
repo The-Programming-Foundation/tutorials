7fc74b8e1f108d95bc444f8e9c1b8c0c3c3da5b6
Subhajeet Mukherjee <subha@smarterus.org>
2022-02-24 17:49:10 +0530

    remove cached

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/home/subha/Downloads/tutorials/node_modules/@docusaurus/plugin-google-analytics/lib/analytics'),
-  require('/home/subha/Downloads/tutorials/node_modules/infima/dist/css/default/default.css'),
-  require('/home/subha/Downloads/tutorials/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/subha/Downloads/tutorials/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/home/subha/Downloads/tutorials/src/css/custom.css'),
-];
